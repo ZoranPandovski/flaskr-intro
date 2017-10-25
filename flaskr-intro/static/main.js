e108e04713d452c6c3df7b6c7a77c0ebf220febb
Zoran Pandovski <zoran.pandovski@keitaro.com>
2017-10-26 00:22:03 +0200

    Add js

---
+++ flaskr-intro/static/main.js
@@ -0,0 +1,17 @@
+(function() {
+  $('.entry').on('click', function() {
+    var entry = this;
+    var post_id = $(this).find('h2').attr('id');
+    $.ajax({
+      type:'GET',
+      url: '/delete' + '/' + post_id,
+      context: entry,
+      success:function(result) {
+        if(result.status === 1) {
+          $(this).remove();
+        }
+      }
+    });
+  });
+
+})($);
