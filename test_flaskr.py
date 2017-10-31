7d1ad5f9c67eceed554d81f8296f074f08c2fcce
Shaphil <shaphil2025@gmail.com>
2017-10-31 22:40:19 +0600

    Refactor test_flaskr

--- test_flaskr.py
+++ test_flaskr.py
@@ -1,14 +1,18 @@
-import unittest
 import os
+import unittest
+
 from flask import json
 
 from flaskr_intro import app, db
 
 TEST_DB = 'test.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+APP_NAME = "flaskr_intro"
+APP_DB = "flaskr.db"
+DB_PATH = os.path.join(basedir, APP_NAME, APP_DB)
 
 
 class BasicTestCase(unittest.TestCase):
-
     def test_index(self):
         """initial test. ensure flask was set up correctly"""
         tester = app.test_client(self)
@@ -17,18 +21,16 @@ class BasicTestCase(unittest.TestCase):
 
     def test_database(self):
         """initial test. ensure that the database exists"""
-        tester = os.path.exists("flaskr.db")
+        tester = os.path.exists(DB_PATH)
         self.assertTrue(tester)
 
 
 class FlaskrTestCase(unittest.TestCase):
-
     def setUp(self):
         """Set up a blank temp database before each test"""
-        basedir = os.path.abspath(os.path.dirname(__file__))
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-            os.path.join(basedir, TEST_DB)
+                                                os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
         db.create_all()
 
@@ -63,7 +65,7 @@ class FlaskrTestCase(unittest.TestCase):
         rv = self.login(app.config['USERNAME'] + 'x', app.config['PASSWORD'])
         self.assertIn(b'Invalid username', rv.data)
         rv = self.login(app.config['USERNAME'], app.config['PASSWORD'] + 'x')
-        self.assertIn(b'Invalid password', rv.data)
+        self.assertIn(b'Invalid username or password', rv.data)
 
     def test_messages(self):
         """Ensure that user can post messages"""
